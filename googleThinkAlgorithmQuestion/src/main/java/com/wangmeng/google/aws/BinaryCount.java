959d7b497a242964e15d122ffe3f6da69e8200de
wangmeng <wm15257140735@163.com>
2019-07-04 23:11:34 +0800

    获取一个数的1的个数，x & x-1

---
+++ googleThinkAlgorithmQuestion/src/main/java/com/wangmeng/google/aws/BinaryCount.java
@@ -0,0 +1,21 @@
+package com.wangmeng.google.aws;
+
+/**
+ * Created by IntelliJ IDEA
+ * USER Administrator
+ * DATE 2019/7/4
+ * TIME 22:04
+ * Description no Description
+ **/
+public class BinaryCount {
+
+    public static void main(String[] args) {
+        int count = 0;
+        int x =  16;
+        while(x > 0){
+            count ++;
+            x &= (x-1);
+        }
+        System.out.println("count :"+count);
+    }
+}
